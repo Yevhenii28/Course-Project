4b2482c33710bf3ed79bfbbde3b2112e51645e05
Yevhenii Kostomarov <zkostomarov7@gmail.com>
2024-06-05 23:31:58 +0300

    Change link to method index and add createUser method

--- src/main/java/kostomarov/courseproject/controllers/MainController.java
+++ src/main/java/kostomarov/courseproject/controllers/MainController.java
@@ -1,5 +1,7 @@
 package kostomarov.courseproject.controllers;
 
+import kostomarov.courseproject.models.Role;
+import kostomarov.courseproject.models.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,8 +13,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MainController {
 
-    @GetMapping("/index")
+    @GetMapping("/main")
     private String index(Model model) {
         return "index";
+    }
+
+    @GetMapping("/createUser")
+    private String createUser(Model model) {
+        User user = new User();
+        Role role = new Role();
+        role.setName("ROLE_ADMIN");
+        user.setRole(role);
+        model.addAttribute("user", user);
+        return "createUser";
     }
 }
