24114046841ea77316cedc6d5a84ff0b3b5b5c90
Yevhenii Kostomarov <zkostomarov7@gmail.com>
2024-06-24 20:04:46 +0300

    Modify controller for edit and delete user.

--- src/main/java/kostomarov/courseproject/controllers/AdminController.java
+++ src/main/java/kostomarov/courseproject/controllers/AdminController.java
@@ -11,7 +11,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
@@ -50,7 +52,40 @@ public class AdminController {
             session.setAttribute("password", rawPassword);
             session.setAttribute("email", user.getEmail());
 
-            return "redirect:/sendMail";
+            return "redirect:/sendLoginMail";
         }
+    }
+
+    @GetMapping("/user/{id}/edit")
+    public String editUser(Model model, @PathVariable Long id) {
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "changeUser";
+    }
+
+    @GetMapping("/editUser/check")
+    public String checkEditUser(Model model, User user, HttpSession session, @RequestParam(name="noChange", required = false) Boolean noChange) {
+        String login = translite.transliterate(user.getSurname() + user.getName());
+        user.setUsername(login);
+        String rawPassword = "";
+        if (noChange == null) {
+            rawPassword = generator.generatePassword();
+            String password = passwordEncoder.encode(rawPassword);
+            user.setPassword(password);
+            userService.createNewUser(user, user.getRole().getName());
+        }
+
+        session.setAttribute("login", login);
+        session.setAttribute("password", rawPassword);
+        session.setAttribute("email", user.getEmail());
+
+        return "redirect:/sendChangeMail";
+    }
+
+    @GetMapping("/user/{id}/delete")
+    public String deleteUser(@PathVariable Long id) {
+        User user = userService.findById(id);
+        userService.deleteUser(user);
+        return "redirect:/index";
     }
 }
