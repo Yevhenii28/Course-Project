6e90ce57f40fa976699ae03c299fbcd7bed17cd9
Yevhenii Kostomarov <zkostomarov7@gmail.com>
2024-06-27 20:37:53 +0300

    Rename Vacation to Absence.

---
+++ src/main/java/kostomarov/courseproject/services/AbsenceService.java
@@ -0,0 +1,34 @@
+package kostomarov.courseproject.services;
+
+import kostomarov.courseproject.models.Absence;
+import kostomarov.courseproject.repositories.AbsenceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AbsenceService {
+    private final AbsenceRepository absenceRepository;
+
+    public List<Absence> getAbsences() {
+        return absenceRepository.getAbsences();
+    }
+
+    public List<Absence> getAbsencesSorted() {
+        return absenceRepository.getAbsencesSorted();
+    }
+
+    public List<Absence> getAbsencesByEmployeeId(Long id) {
+        return absenceRepository.getAbsencesByEmployee_Id(id);
+    }
+
+    public List<Absence> getAbsencesByEmployeeSurname(String surname) {
+        return absenceRepository.getAbsenceByEmployee_Surname(surname);
+    }
+
+    public List<Absence> getAbsencesByDate(int year) {
+        return absenceRepository.getAbsencesByDateYear(year);
+    }
+}
