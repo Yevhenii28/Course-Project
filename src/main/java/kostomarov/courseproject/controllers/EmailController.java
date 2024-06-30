1bcd3a8b15729f5cb7e458e306250d828abe1308
Yevhenii Kostomarov <zkostomarov7@gmail.com>
2024-06-30 18:28:32 +0300

    Add method for changing password mail.

--- src/main/java/kostomarov/courseproject/controllers/EmailController.java
+++ src/main/java/kostomarov/courseproject/controllers/EmailController.java
@@ -13,22 +13,43 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequiredArgsConstructor
 public class EmailController {
     private final EmailService emailService;
-    private final UserService userService;
 
-    @GetMapping("/sendMail")
+    @GetMapping("/sendLoginMail")
     public String sendLoginMail(HttpSession session) {
         String login = (String) session.getAttribute("login");
         String password = (String) session.getAttribute("password");
         String email = (String) session.getAttribute("email");
 
         String subject = "Ваші дані для входу";
-        String text = String.format("Ваші дані для входу в систему WFM:\nЛогін: %s\nПароль: %s", login, password);
+        String text = String.format("Ваші дані для входу в систему EAS:\nЛогін: %s\nПароль: %s", login, password);
         emailService.sendTextMail("zkostomarov7@gmail.com", subject, text);
 
         session.removeAttribute("login");
         session.removeAttribute("password");
         session.removeAttribute("email");
 
-        return "redirect:/main";
+        return "redirect:/index";
+    }
+
+    @GetMapping("/sendChangeMail")
+    public String sendChangeMail(HttpSession session) {
+        String login = (String) session.getAttribute("login");
+        String password = (String) session.getAttribute("password");
+        String email = (String) session.getAttribute("email");
+
+        String subject = "Ваші оновлені дані для входу";
+        String text = "";
+        if (password.equals("")) {
+            text = String.format("Ваші дані для входу в систему EAS:\nЛогін: %s\nПароль: не змінений", login);
+        } else {
+            text = String.format("Ваші дані для входу в систему EAS:\nЛогін: %s\nПароль: %s", login, password);
+        }
+        emailService.sendTextMail("zkostomarov7@gmail.com", subject, text);
+
+        session.removeAttribute("login");
+        session.removeAttribute("password");
+        session.removeAttribute("email");
+
+        return "redirect:/index";
     }
 }
